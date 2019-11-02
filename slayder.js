9f4422f351e3979065e678dedd408d654dd6fbe8
Richpanda006 <47279995+Richpanda006@users.noreply.github.com>
2019-11-02 15:51:14 +0400

    Update slayder.js

--- slayder.js
+++ slayder.js
@@ -1,9 +1,15 @@
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
-
+var notRegister = 0;
 function nextSlide() {
  slides[currentSlide].className = 'slide';
  currentSlide = (currentSlide+1)%slides.length;
  slides[currentSlide].className = 'slide showing';
+}
+if(notRegister<=0){
+  alert("Вы первый посетитель")
+}
+else{
+  console.log("#")
 }
